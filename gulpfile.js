2c94405f5d1b8bf4f0db0ff4a9229f52228c01c6
sami <samialzate@hotmail.com>
2023-05-30 23:44:57 -0500

    creando la funcion de css en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-s 
+const { src,dest} = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+
+function css( done) {
+//compilar sass
+//pasos: 1 identificar archivo, 2 compilarla, 3 guardar el .css
+    src('src/scss/app.scss')
+    .pipe(sass ())
+    .pipe( dest ('build/css'))
+
+    done();
+}
+
+exports.css = css;
