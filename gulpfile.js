f7eb751a434a0bbe623030411796ee7fab708bdd
sami <samialzate@hotmail.com>
2023-05-31 23:55:46 -0500

    Craando variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
-const { src,dest} = require("gulp");
+const { src,dest, watch, series, parallel } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-function css( done) {
+function css( done ) {
 //compilar sass
 //pasos: 1 identificar archivo, 2 compilarla, 3 guardar el .css
     src('src/scss/app.scss')
-    .pipe(sass ())
-    .pipe( dest ('build/css'))
+    .pipe( sass() )
+    .pipe( postcss ( [ autoprefixer() ] ) )
+    .pipe( dest ('build/css') )
 
     done();
 }
 
+function dev() {
+    watch( 'src/scss/**/*.scss', css);
+ 
+}
+
+function tareaDefault() {
+    console.log( 'soy la tarea por default');
+}
+
 exports.css = css;
+exports.dev = dev;
+exports.default = tareaDefault;
