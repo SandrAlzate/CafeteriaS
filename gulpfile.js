a3df34ea1da46d0fa52c1aba727b3abf9a503668
sami <samialzate@hotmail.com>
2023-06-06 23:25:22 -0500

    culminación del header

--- gulpfile.js
+++ gulpfile.js
@@ -22,19 +22,26 @@ function css( done ) {
 }
 function imagenes( ) {
     return src('src/img/**/*')
-     .pipe( imagemin({ optimiacionLevel: 3 }) )
+     .pipe( imagemin({ optimizationLevel: 3 }) )
      .pipe( dest('build/img') )
 }
 
 function versionWebp() {
+    const opciones = {
+        quality:50
+    }
     return src('src/img/**/*.{png,jpg}')
-    .pipe( webp() )
+    .pipe( webp(opciones) )
     .pipe( dest('build/img') )
 }
 
 function versionAvif() {
+    const opciones = {
+        quality:50
+    }
     return src('src/img/**/*.{png,jpg}')
     .pipe( avif())
+    .pipe( dest('build/img')   )
 }
 
 function dev() {
@@ -51,4 +58,5 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series( imagenes, versionWebp, css, dev );
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
